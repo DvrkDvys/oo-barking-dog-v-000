79f316adf6f84c4a6a31b19be74abac127cebaf8
Jordan Harris <jharris@wework.com>
2018-06-19 18:30:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,21 @@
 # Your code goes here!
 class Dog
+  def name=(fido)
+    @name = fido
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(beagle)
+    @breed = beagle
+  end
+
+  def breed
+    @breed
+  end
+  
   def bark
     puts "Woof!"
   end
